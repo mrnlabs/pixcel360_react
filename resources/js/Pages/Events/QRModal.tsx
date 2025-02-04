7c4be80a06829f305ee5341814edf3034a224cb9
mazisi msebele <mazisimsebele18@gmail.com>
2025-02-04 21:54:56 +0200

    feat: Add QR code functionality in QRModal and implement scroll-to-top button

--- resources/js/Pages/Events/QRModal.tsx
+++ resources/js/Pages/Events/QRModal.tsx
@@ -1,4 +1,5 @@
 
+import { Button } from "@/Components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -7,6 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/Components/ui/dialog";
+import { QRCodeSVG } from "qrcode.react";
 
 function QRModal({
     open,
@@ -19,10 +21,21 @@ function QRModal({
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent className="sm:max-w-[425px] top-[15%] translate-y-0">
                 <DialogHeader>
-                    <DialogTitle className="card-title">QR Modal</DialogTitle>
+                    <DialogTitle className="card-title">Activate Event</DialogTitle>
                     <DialogDescription></DialogDescription>
                 </DialogHeader>
-                QR Modal
+                <div className="flex justify-center items-center">
+                <QRCodeSVG
+                    value={"https://reactjs.org/"}
+                    title={"Title for my QR Code"}
+                    size={128}
+                    bgColor={"#ffffff"}
+                    fgColor={"#000000"}
+                    level={"M"}
+                    minVersion={6}
+                    />
+                </div>
+                <Button className="ti-btn ti-btn-primary btn-wave  waves-effect waves-light">Close</Button>
             </DialogContent>
         </Dialog>
     );
--- resources/js/Shared/Footer.tsx
+++ resources/js/Shared/Footer.tsx
@@ -1,14 +1,42 @@
+import { Link } from '@inertiajs/react'
+import { MoveUp } from 'lucide-react'
 import React from 'react'
 
 export default function Footer() {
+// if page view is top hide scroll to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    const scrollToTopButton = document.querySelector(".scrollToTop");
+    if (scrollToTopButton) {
+    const scrollToTopButton = document.querySelector(".scrollToTop") as HTMLElement;
+    if (scrollToTopButton) {
+      scrollToTopButton.style.display = "none";
+    }
+    }
+  } else {
+    const scrollToTopButton = document.querySelector(".scrollToTop") as HTMLElement;
+    if (scrollToTopButton) {
+      scrollToTopButton.style.display = "none";
+    }
+  }
+}
   return (
+    <>
     <footer className="mt-auto py-4 bg-white dark:bg-bodybg text-center border-t border-defaultborder dark:border-defaultborder/10">
     <div className="container">
       <span className="text-textmuted dark:text-textmuted/50"> Copyright © <span id="year">2025</span>
-        <a href="javascript:void(0);" className="text-dark font-medium">Xintra</a>. Designed with <span className="text-danger">❤</span> by <a href="javascript:void(0);">
-          <span className="font-medium text-primary">Spruko</span>
+        <Link href="javascript:void(0);" className="text-dark font-medium">Pixcel</Link>. Designed with <span className="text-danger">❤</span> by <a href="https://mrnlabs.com" target="_blank">
+          <span className="font-medium text-primary">mrnlabs</span>
         </a> All rights reserved </span>
     </div>
   </footer>
+  <div onClick={() => window.scrollTo(0, 0)} className="scrollToTop cursor-pointer" > 
+    <span className="arrow">
+    <MoveUp className='text-xl' />
+    </span> 
+    </div>
+    </>
   )
 }
