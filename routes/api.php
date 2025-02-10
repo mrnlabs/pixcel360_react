d9c8c57e5f226d0695cdb847fe43d92d0a193c1f
Mazisi Msebele <56942052+mazisi@users.noreply.github.com>
2025-02-10 16:12:38 +0200

    feat: Add cache clearing and storage linking endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use App\Http\Controllers\API\EventAPIController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('storage:link');
+    Artisan::call('config:cache');
+    return "success";
+});
 
 Route::post('/active-event', [EventAPIController::class, 'activateEvent'])->name('event.activate');
 Route::post('/upload-event-video', [EventAPIController::class, 'uploadVideo'])->name('event.upload.video');
