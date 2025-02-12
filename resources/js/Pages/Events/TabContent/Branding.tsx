9edd8a443851a87555f44ce8a36ad3aaa29355a1
mazisi msebele <mazisimsebele18@gmail.com>
2025-02-12 14:21:38 +0200

    fix: Update logo size in sidebar

--- resources/js/Pages/Events/TabContent/Branding.tsx
+++ resources/js/Pages/Events/TabContent/Branding.tsx
@@ -1,6 +1,4 @@
-import CustomToggle from '@/Components/Form/CustomToggle'
 import { Input } from '@/Components/ui/input'
-import { Textarea } from '@headlessui/react'
 import Logo from './Logo'
 import { Button } from '@/Components/ui/button'
 export default function Branding({event} : any) {
@@ -17,12 +15,6 @@ export default function Branding({event} : any) {
      <div className="xl:col-span-6 lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12">
       <label className='block text-sm mb-1' htmlFor="def-email-subject">Text and button color</label>
       <input type='color' className='p-1 h-10 w-10 block bg-white dark:bg-bodybg border border-gray-200 cursor-pointer rounded-sm'/>
-     </div>
-     <div className="xl:col-span-12 lg:col-span-12 md:col-span-12 sm:col-span-12 col-span-12">
-      <label className='block text-sm mb-1' htmlFor="def-message">Text message (SMS)</label>
-     <textarea rows={2} id='def-message' className="w-full px-3 py-2 border rounded-lg" placeholder="Enter sms text message"
-        onChange={(value) => console.log('Toggle value:', value)} 
-        ></textarea>
      </div>
   </div>
      <Button className='mt-4 ti-btn ti-btn-primary w-full'>Save</Button>
